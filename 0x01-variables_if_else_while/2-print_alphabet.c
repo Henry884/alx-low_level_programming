95169163ab154559660ec1cc93f177a805613403
Henry884 <henrynwakamaa@gmail.com>
2022-12-12 06:22:00 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,32 +17,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-00 : 36
-#include <stdio.h>
-#include <ctype.h>
-/**
-*main - Program to print alphabet letters in all cases followed by new line
-*
-*Return: return 0
-*/
-int main(void)
-{
-	int letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		letter = tolower(letter);
-		putchar(letter);
-		if (letter == 'z')
-		{
-			letter = 'A';
-			for (; letter <= 'Z'; letter++)
-			{
-				putchar(letter);
-			}
-			break;
-		}
-	}
-	putchar('\n');
-	return (0);
-}
